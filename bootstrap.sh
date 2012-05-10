13286e47b6e580f0a5691e84d0e6a107a804f014
David Brenneman <db@davidbrenneman.com>
2012-05-10 10:49:51 -0700

    added updated bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,10 @@ cd "$(dirname "$0")"
 git pull
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+    cd ~/.emacs.d/plugins
+    git clone https://github.com/capitaomorte/yasnippet
+    cd ~/.emacs.d/themes
+    git clone https://github.com/sellout/emacs-color-theme-solarized
 }
 if [  "$1" == "--force" -o "$1" == "-f" ]; then
     doIt
