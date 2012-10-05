d9193ca5783280c514f4fc16dcd9af42350555ca
David Brenneman <db@davidbrenneman.com>
2012-10-05 05:14:57 +0000

    added nxhtml to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,33 +3,37 @@ cd "$(dirname "$0")"
 git pull
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
-    cd ~/.emacs.d/plugins
-    git clone https://github.com/fgallina/python.el.git python
-    cd ~/.emacs.d/plugins/python
-    git pull
-    cd ~/.emacs.d/plugins
-    git clone https://github.com/capitaomorte/yasnippet
-    cd ~/.emacs.d/plugins/yasnippet
-    git pull
-    cd ~/.emacs.d/plugins
-    git clone https://github.com/magit/magit
-    cd ~/.emacs.d/plugins/magit
-    git pull
-    cd ~/.emacs.d/themes
-    git clone https://github.com/sellout/emacs-color-theme-solarized
-    cd ~/.emacs.d/themes/emacs-color-theme-solarized
-    git pull
-    cd ~/.emacs.d/themes
-    git clone https://github.com/bbatsov/zenburn-emacs.git
-    cd ~/.emacs.d/themes/zenburn-emacs
-    git pull
     # nXhtml mode for web development
     cd ~/.emacs.d/plugins
     rm -rf ./nxhtml
     wget http://ourcomments.org/Emacs/DL/elisp/nxhtml/zip/nxhtml-2.08-100425.zip
     unzip nxhtml-2.08-100425.zip
     rm nxhtml-2.08-100425.zip
-    mv nxhtml-2.08-100425 nxhtml
+    # python mode
+    cd ~/.emacs.d/plugins
+    git clone https://github.com/fgallina/python.el.git python
+    cd ~/.emacs.d/plugins/python
+    git pull
+    # yasnippet
+    cd ~/.emacs.d/plugins
+    git clone https://github.com/capitaomorte/yasnippet
+    cd ~/.emacs.d/plugins/yasnippet
+    git pull
+    # magit
+    cd ~/.emacs.d/plugins
+    git clone https://github.com/magit/magit
+    cd ~/.emacs.d/plugins/magit
+    git pull
+    # solarized theme
+    cd ~/.emacs.d/themes
+    git clone https://github.com/sellout/emacs-color-theme-solarized
+    cd ~/.emacs.d/themes/emacs-color-theme-solarized
+    git pull
+    # zenburn theme
+    cd ~/.emacs.d/themes
+    git clone https://github.com/bbatsov/zenburn-emacs.git
+    cd ~/.emacs.d/themes/zenburn-emacs
+    git pull
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
