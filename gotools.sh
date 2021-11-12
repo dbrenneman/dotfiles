b345bb6f3490b429e4fe71eed343332d587c8897
David Brenneman <dbrenneman@HeartOfGold.local>
2021-11-12 14:42:12 -0800

    update gotools install script

--- gotools.sh
+++ gotools.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
 echo "Installing gopls..."
-GO111MODULE=on go get golang.org/x/tools/gopls
-
-echo "Installing golangci-lint..."
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.42.0
+go install golang.org/x/tools/gopls@latest
 
 echo "Installing goconvey..."
-go get github.com/smartystreets/goconvey
-
-echo "Installing protoc-gen-go..."
-go get google.golang.org/protobuf/cmd/protoc-gen-go@latest
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install github.com/smartystreets/goconvey@latest
